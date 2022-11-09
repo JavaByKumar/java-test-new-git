99998bf11297266b9764bd34fe1b12b26ffadb4d
Kumar <Kumar@gmail.com>
2022-11-09 13:50:44 +0530

    added multiply.java file

---
+++ Mulitply.java
@@ -0,0 +1,10 @@
+public class Mulitply {
+    public static void main(String args[]){
+        int x,y,z;
+        x=10;
+        y=20;
+        z=x*y;
+        System.out.println("Multi is:"+z);
+    }
+    
+}
